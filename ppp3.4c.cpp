72fbd3a527f968826b76bdb0926d603fb73c74ea
Khnumhotep <98873452+Khnumhotep@users.noreply.github.com>
2023-12-06 05:45:41 +0300

    Add files via upload
    a sample cpp file

---
+++ ppp3.4c.cpp
@@ -0,0 +1,15 @@
+// read and compare names in terms of which comes first
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Please enter two names\n ";
+string first;
+string second;
+cin >> first >> second; // read two strings
+if (first == second) cout << "that's the same name twice\n ";
+if (first < second)
+cout << first << " is alphabetically before " << second <<'\n ' ;
+if (first > second)
+cout << first << " is alphabetically after " << second <<'\n';
+}
